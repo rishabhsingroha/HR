4fd6ceda7e831a5024f50bef9504a48ff1b82c39
Rishabh <141716230+rishabhsingroha@users.noreply.github.com>
2025-07-24 10:47:38 +0530

    4th

--- call_handler.py
+++ call_handler.py
@@ -15,10 +15,6 @@ class CallHandler:
         self.client = Client(self.account_sid, self.auth_token)
         self.tts = TextToSpeech()
         self.questions = [
-            "Can you tell me about yourself?",
-            "What are your key skills?",
-            "How many years of experience do you have?",
-            "What is your current location?",
             
         ]
 
